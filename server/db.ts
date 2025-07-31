aaaf2b1e8c70a38088a3816571eab620de0bb6bf
isaacenerva <isaac.ullah@enerva.ca>
2025-07-31 14:41:18 -0600

    DB Setup Moved to Supabase

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,25 @@
-import 'dotenv/config';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
-import * as schema from "@shared/schema";
+// import 'dotenv/config';
+// import { Pool, neonConfig } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-serverless';
+// import ws from "ws";
+// import * as schema from "@shared/schema";
 
-neonConfig.webSocketConstructor = ws;
+// neonConfig.webSocketConstructor = ws;
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error(
+//     "DATABASE_URL must be set. Did you forget to provision a database?",
+//   );
+// }
+
+// export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// export const db = drizzle({ client: pool, schema });
+
+
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
@@ -12,5 +27,6 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+// Create postgres connection
+export const client = postgres(process.env.DATABASE_URL);
+export const db = drizzle({ client, schema });
